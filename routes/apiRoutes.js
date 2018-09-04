1707e860d3ff147274afe5c35134309ac06a24ef
Joseph V <joseph.vonedwins@yahoo.com>
2018-09-04 17:04:40 -0400

    uploading files for authentication and routing

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,100 @@
+//require bcrypt as our hashing algorithm
+var bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+//require passport
+var passport = require("passport");
+
+//require the databse
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/api/examples", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.json(dbExamples);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  // Post route to add a new user to the database and send the new user to the survey
+  //The path for this route should match the path for the submit button on the register a new user
+  app.post("/api/createnewuser", function (req, res) {
+
+    //read in data from the submission from the registration page
+  
+
+    //hash the password with bcrypt for secure storage in the databse
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+
+      newUser = {
+        email: req.body.userEmail,
+        password: hash
+      };
+
+      console.log(newUser.password);
+
+      //sequelize send info to database
+      //need to match name to model name, placeholder as user for now
+      db.User.create(newUser).then(function (dbUser) {
+
+        var userId = dbUser.id;
+
+        //logs in user if profile creation succeeds
+        if (userId) {
+          console.log('login Id: ' + userId);
+          req.login(userId, function(err) {
+            var newPath = "/survey"// + "/" +userId //uncomment when individual id paths are ready
+
+            res.json({
+              "redirect": true,
+              "redirect_url": newPath 
+            });
+          });
+        }
+
+      });
     });
+  });//end register a user
+
+
+  //This route logs a user in from the login page
+  app.post("/api/authenticateuser", passport.authenticate("local", {
+    successRedirect: '/',
+    failureRedirect: '/login'
+  }));
+
+  app.get('/logout', function(req, res) {
+    req.logout();
+    req.session.destroy();
+    res.redirect('/');
   });
 
+
+
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.delete("/api/examples/:id", function (req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
       res.json(dbExample);
     });
   });
+};
+
+
+passport.serializeUser(function(userId, done) {
+  done(null, userId);
+});
+ 
+passport.deserializeUser(function(userId, done) {
+    done(null, userId);
+});
+
+
+//call this function to allow access only to signed in and authenticated users.
+function authenticationMiddleware() {
+  return (req, res, next) => {
+
+    if (req.isAuthenticated()) return next();
+
+    res.redirect('/login');
+  };
 };
--- server.js
+++ server.js
@@ -2,8 +2,18 @@ require("dotenv").config();
 var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
+var cookieParser = require("cookie-parser");
+
+//Authentication packages
+var session = require("express-session");
+var passport = require("passport");
+var LocalStrategy = require('passport-local').Strategy;
+var MySQLStore = require("express-mysql-session")(session);
+var bcrypt = require('bcrypt');
+
 
 var db = require("./models");
+
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -12,6 +22,61 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
+app.use(cookieParser());
+
+//sets up approptiate to allow sessions to continue
+var options = {
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "exampledb",
+  port: 8889,
+};
+var sessionStore = new MySQLStore(options);
+
+//set use for express-session
+//this portion creates the cookie for each session
+app.use(session({
+  secret: "lotsofwordsandstuffthiscanberandomtext",//this is like a salt for a hashing function. It should be random text
+  resave: false, //session only saves when a change is made
+  store: sessionStore,
+  saveUninitialized: false, //only creates a cookie if the user logs in
+  //cookie: {secure: true}
+}));
+//sets up use for authentication
+app.use(passport.initialize());
+app.use(passport.session());
+
+//tells passport we are using a local databse for authentication checks
+passport.use(new LocalStrategy(function (username, password, done) {
+  console.log(username);//this needs to be called username
+  console.log(password);// this needs to be called password
+
+  db.User.findOne({
+    where: {
+      email: username
+    }
+  }).then(function (dbUser) {
+    if (!dbUser) {
+      console.log("not a valid user");
+      return (null, false);
+    };
+    console.log("valid user");
+    const hash = dbUser.password;
+
+
+    bcrypt.compare(password, hash, function (err, response) {
+      if (err) throw err;
+      if (response === true) {
+        return done(null, {userId: dbUser.id});
+      } else {
+        return done(null, false);
+      }
+    });
+
+  });//end database call to get user trying to login
+
+})); //end passport local-strategy set up
 
 // Handlebars
 app.engine(
@@ -35,8 +100,8 @@ if (process.env.NODE_ENV === "test") {
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync(syncOptions).then(function () {
+  app.listen(PORT, function () {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
