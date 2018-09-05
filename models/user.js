c8473aba97d32e09e0e96260cf387770a50c3613
CSneed7 <37349534+CSneed7@users.noreply.github.com>
2018-09-05 17:38:35 -0400

    Update user.js

--- models/user.js
+++ models/user.js
@@ -4,8 +4,19 @@ module.exports = function(sequelize, DataTypes) {
       password: DataTypes.STRING,
       email: DataTypes.STRING,
       photo: DataTypes.STRING,
-      location: DataTypes.STRING,    
+      location: DataTypes.STRING, 
+      age: DataTypes.INTEGER,
+      question1: DataTypes.INTEGER,
+      question2: DataTypes.INTEGER,
+      question3: DataTypes.INTEGER,
+      question4: DataTypes.INTEGER,
+      question5: DataTypes.INTEGER,
+      question6: DataTypes.INTEGER,
+      question7: DataTypes.INTEGER,
+      question8: DataTypes.INTEGER,
+      question9: DataTypes.INTEGER,
+      question10: DataTypes.INTEGER
     });
     return User;
-  };
-
+  }; 
+ 
